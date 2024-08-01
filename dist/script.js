581915b54f52629f4b355b62999503784053b4fa
SachinKiragi <sachinkiragi8@gmail.com>
2024-08-01 21:45:01 +0530

    connect all individual files

---
+++ dist/script.js
@@ -0,0 +1,29 @@
+// My api key api ninjas
+// MyH1I1/AI/noVIetsno/Rg==mQz81QE8MwqOtC03
+const result = document.getElementById('result');
+
+
+async function getQuote(){
+
+    try{
+
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+            method: 'GET',
+            headers: { 'X-Api-Key': 'MyH1I1/AI/noVIetsno/Rg==mQz81QE8MwqOtC03' },
+            contentType: 'application/json'
+    });
+
+    let data = await response.json();
+
+    
+    result.innerHTML = `
+        <p class="quote" id="quote">${data[0].quote}</p>
+
+        <h1 class="font-bold">${ data[0].author}</h1>
+    `
+    } catch(e){
+        console.log("could not find quote");
+    }
+
+    
+}
